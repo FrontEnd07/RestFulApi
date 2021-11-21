0a26ffb762932efd3ad6439159bb0b54a774e538
FrontEnd07 <yunusov9433@gmail.com>
2021-11-21 15:08:56 +0500

    upload practic project in github

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./Router.js";
+
+const PORT = 5000;
+const DB_URL = `mongodb+srv://nodejs:927261033y@cluster0.15wgj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const app = express();
+app.use(express.json());
+app.use("/api", router);
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true});
+        app.listen(PORT, () => console.log("SERVER STARTED ON PORT " + PORT));
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+startApp()
