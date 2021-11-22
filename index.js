4282d23f60ee37ebc224146121f7c36dac1ba467
FrontEnd07 <yunusov9433@gmail.com>
2021-11-22 13:46:18 +0500

    create fn file upload

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./Router.js";
+import fileUpload from "express-fileupload";
 
 const PORT = 5000;
 const DB_URL = `mongodb+srv://nodejs:927261033y@cluster0.15wgj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const app = express();
+
 app.use(express.json());
+app.use(express.static("static"));
+app.use(express.urlencoded({extended: true}))
+app.use(fileUpload({}));
 app.use("/api", router);
+
 
 async function startApp() {
     try {
